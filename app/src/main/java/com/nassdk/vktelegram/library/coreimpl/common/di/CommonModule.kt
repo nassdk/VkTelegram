aa697ef4ce954f520850f4cb1c079bb1dafb2d1a
nassdk <nassdk17@gmail.com>
2021-05-17 23:30:42 +0300

    Added hilt inject for data store.

--- app/src/main/java/com/nassdk/vktelegram/library/coreimpl/common/di/CommonModule.kt
+++ app/src/main/java/com/nassdk/vktelegram/library/coreimpl/common/di/CommonModule.kt
@@ -1,11 +1,14 @@
 package com.nassdk.vktelegram.library.coreimpl.common.di
 
+import android.content.Context
 import com.nassdk.vktelegram.library.coreapi.common.error.ErrorHandler
 import com.nassdk.vktelegram.library.coreapi.common.resourcemanager.ResourceManager
+import com.nassdk.vktelegram.library.coreimpl.common.data.DataStorage
 import com.nassdk.vktelegram.library.coreimpl.common.error.ErrorHandlerImpl
 import com.nassdk.vktelegram.library.coreimpl.common.resourcemanager.ResourceManagerImpl
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
@@ -21,4 +24,11 @@ abstract class CommonModule {
     @Binds
     @Singleton
     abstract fun bindResourceManager(impl: ResourceManagerImpl): ResourceManager
+
+    companion object {
+
+        @Provides
+        @Singleton
+        fun provideDataStore(@ApplicationContext context: Context) = DataStorage(context = context)
+    }
 }
