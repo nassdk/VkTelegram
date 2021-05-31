4717f27aa02de40d5451983f34054d9a20ce3a30
nassdk <nassdk17@gmail.com>
2021-05-31 22:32:10 +0300

    Added transition to videos if stored token exist.

--- app/src/main/java/com/nassdk/vkvideo/feature/splash/SplashFragment.kt
+++ app/src/main/java/com/nassdk/vkvideo/feature/splash/SplashFragment.kt
@@ -3,18 +3,29 @@ package com.nassdk.vkvideo.feature.splash
 import androidx.lifecycle.lifecycleScope
 import com.nassdk.vkvideo.R
 import com.nassdk.vkvideo.feature.auth.AuthFragment
+import com.nassdk.vkvideo.feature.videos.presentation.VideosFragment
+import com.nassdk.vkvideo.library.coreimpl.common.data.DataStorage
 import com.nassdk.vkvideo.library.coreui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SplashFragment : BaseFragment(R.layout.screen_splash) {
+
+    @Inject lateinit var dataStorage: DataStorage
 
     override fun prepareUi() {
 
         lifecycleScope.launchWhenStarted {
             delay(3000)
-            val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.container, AuthFragment())
-            fragmentTransaction.commit()
+
+            requireActivity().supportFragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.container,
+                    if (dataStorage.getAccessToken().isNullOrBlank()) AuthFragment() else VideosFragment()
+                ).commit()
         }
     }
 }
