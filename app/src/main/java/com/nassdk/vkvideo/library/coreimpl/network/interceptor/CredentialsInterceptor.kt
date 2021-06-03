e52c574d6f3a204b839abc7d6b30c98aee8e1b3a
nassdk <nassdk17@gmail.com>
2021-06-03 20:22:27 +0300

    Added request logging.

--- app/src/main/java/com/nassdk/vkvideo/library/coreimpl/network/interceptor/CredentialsInterceptor.kt
+++ app/src/main/java/com/nassdk/vkvideo/library/coreimpl/network/interceptor/CredentialsInterceptor.kt
@@ -1,10 +1,12 @@
 package com.nassdk.vkvideo.library.coreimpl.network.interceptor
 
+import com.nassdk.vkvideo.BuildConfig
 import com.nassdk.vkvideo.library.coreimpl.common.data.DataStorage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
+import timber.log.Timber
 import javax.inject.Inject
 
 class CredentialsInterceptor @Inject constructor(
@@ -27,6 +29,10 @@ class CredentialsInterceptor @Inject constructor(
             .addQueryParameter("v", API_VERSION)
 
         val requestBuilder = originalRequest.newBuilder().url(newUrl.build())
+
+        if (BuildConfig.DEBUG) {
+            Timber.tag("VK_VIDEO_REQUEST_LOG").e(newUrl.toString())
+        }
 
         return chain.proceed(requestBuilder.build())
     }
